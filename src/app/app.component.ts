119974a13feafdf12f3e3c1ee3e8a26d9b260352
Renée Maksoud <renee.maksoud@correiarodrigues.com.br>
2021-12-01 19:37:57 -0300

    Recipe list, details and items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector:    'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:   ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-second-app';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+
+    this.loadedFeature = feature;
+
+  }
+
 }
